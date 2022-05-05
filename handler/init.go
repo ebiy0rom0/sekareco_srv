8d704bc33ffa035cb54631daf0316860cc70b9df
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-05-06 03:49:06 +0900

    [mod]isolate api handler init

---
+++ handler/init.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"sekareco_srv/handler/music"
+	"sekareco_srv/handler/person"
+	"sekareco_srv/handler/record"
+
+	"github.com/gorilla/mux"
+)
+
+func Init() *mux.Router {
+	// create handler rooting
+	r := mux.NewRouter()
+
+	// person api
+	r.HandleFunc("/person/{personId}/", person.Get).Methods("GET")
+	r.HandleFunc("/person/", person.Post).Methods("POST")
+	r.HandleFunc("/person/{personId}/", person.Patch).Methods("PATCH")
+
+	// music api
+	r.HandleFunc("/music/", music.Get).Methods("GET")
+
+	// record api
+	r.HandleFunc("/record/{personId}/", record.Get).Methods("GET")
+	r.HandleFunc("/record/{personId}/", record.Post).Methods("POST")
+	r.HandleFunc("/record/{personId}/{musicId}/", record.Patch).Methods("PATCH")
+
+	return r
+}
