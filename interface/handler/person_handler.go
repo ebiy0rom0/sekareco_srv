4c7382302cb2dd5ea0fb508c2169c9f753fec531
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-05-14 23:36:10 +0900

    [mod]response to errors
    resuponse error status and body

--- interface/handler/person_handler.go
+++ interface/handler/person_handler.go
@@ -1,8 +1,6 @@
 package handler
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"sekareco_srv/domain/dto"
 	"sekareco_srv/domain/model"
@@ -25,44 +23,51 @@ func NewPersonHandler(sqlHandler database.SqlHandler) *PersonHandler {
 	}
 }
 
-func (handler *PersonHandler) Get(c HttpContext) {
-	vars := c.Vars()
+func (handler *PersonHandler) Get(ctx HttpContext) {
+	vars := ctx.Vars()
 	personId, _ := strconv.Atoi(vars["personId"])
 
 	person, err := handler.logic.GetPersonById(personId)
 	if err != nil {
-		log.Printf("%s", err)
+		ctx.Response(http.StatusServiceUnavailable, ctx.MakeError("パーソン情報が取得できません。"))
+		return
 	}
 
-	c.Response(http.StatusOK, person)
+	ctx.Response(http.StatusOK, person)
 }
 
-func (handler *PersonHandler) Post(c HttpContext) {
-	var p map[string]string
-	c.Decode(&p)
-	fmt.Println(p)
-	vo, err := dto.NewRequestRegistPerson(p["login_id"], p["password"], p["person_name"])
+func (handler *PersonHandler) Post(ctx HttpContext) {
+	var req map[string]string
+	if err := ctx.Decode(&req); err != nil {
+		ctx.Response(http.StatusBadRequest, ctx.MakeError("リクエストパラメータの取得に失敗しました。"))
+		return
+	}
+
+	vo, err := dto.NewRequestRegistPerson(req["login_id"], req["password"], req["person_name"])
 	if err != nil {
-		log.Printf("bad request: %s", err)
+		ctx.Response(http.StatusBadRequest, ctx.MakeError("リクエストパラメータが不正です。"))
+		return
 	}
 
 	ok, err := handler.logic.CheckDuplicateLoginId(vo.GetLoginId())
 	if err != nil {
-		log.Printf("%s", err)
+		ctx.Response(http.StatusServiceUnavailable, ctx.MakeError("重複チェックの検証に失敗しました。"))
+		return
 	} else if !ok {
-		log.Printf("duplicate login id: %s", vo.GetLoginId())
+		ctx.Response(http.StatusServiceUnavailable, ctx.MakeError("ログインIDは既に存在しています。"))
+		return
 	}
 
 	handler.logic.Repository.StartTransaction()
 
-	code := handler.logic.GenerateFriendCode(vo.GetLoginId())
+	code, _ := handler.logic.GenerateFriendCode(vo.GetLoginId())
 	person := model.Person{
 		PersonName: vo.GetPersonName(),
 		FriendCode: code,
 	}
 	personId, err := handler.logic.RegistPerson(person)
 	if err != nil {
-		log.Printf("person regist failed: %s", err)
+		ctx.Response(http.StatusServiceUnavailable, ctx.MakeError("パーソン情報の登録に失敗しました。"))
 		handler.logic.Repository.Rollback()
 		return
 	}
@@ -70,7 +75,8 @@ func (handler *PersonHandler) Post(c HttpContext) {
 
 	hash, err := handler.logic.GeneratePasswordHash(vo.GetPassword())
 	if err != nil {
-		log.Printf("password hash generate failed: %s", err)
+		ctx.Response(http.StatusServiceUnavailable, ctx.MakeError("パスワードハッシュの生成に失敗しました。"))
+		return
 	}
 	login := model.Login{
 		LoginId:      vo.GetLoginId(),
@@ -78,27 +84,26 @@ func (handler *PersonHandler) Post(c HttpContext) {
 		PasswordHash: hash,
 	}
 
-	err = handler.logic.RegistLogin(login)
-	if err != nil {
-		log.Printf("login regist failed: %s", err)
+	if err := handler.logic.RegistLogin(login); err != nil {
+		ctx.Response(http.StatusServiceUnavailable, ctx.MakeError("ログイン情報の登録に失敗しました。"))
 		handler.logic.Repository.Rollback()
 		return
 	}
 
 	handler.logic.Repository.Commit()
 
-	c.Response(http.StatusCreated, person)
+	ctx.Response(http.StatusCreated, person)
 }
 
 // TODO: Implement
-func (handler *PersonHandler) Put(c HttpContext) {
+func (handler *PersonHandler) Put(ctx HttpContext) {
 
-	vars := c.Vars()
+	vars := ctx.Vars()
 
 	// @debug
 	vars["uri"] = "person"
 	vars["methods"] = "put"
 	// output, _ := json.Marshal(vars)
 
-	c.Response(http.StatusOK, vars)
+	ctx.Response(http.StatusOK, vars)
 }
