a715c8a3908aa213dc121ae645185c9ebb9f2d20
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-12 19:43:29 +0900

    [mod]change insert, update date column
    from _date to _at

--- doc/db/friend_ddl.sql
+++ doc/db/friend_ddl.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS friend (
     person_id INTEGER NOT NULL,
     friend_person_id INTEGER NOT NULL,
     is_compare INTEGER NOT NULL DEFAULT 0,
-    insert_date TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
-    update_date TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+    inserted_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
 CREATE INDEX idx_friend_1 ON friend (
     person_id
