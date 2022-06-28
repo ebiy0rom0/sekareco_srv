fe956092fe5c82b0dfc6e29f672e6169d35ca752
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-06-28 19:30:27 +0900

    [mod]token operate
    add, remove and enable check

--- infra/middleware/auth.go
+++ infra/middleware/auth.go
@@ -3,3 +3,21 @@ package middleware
 // access token mapping
 // key: personID, value: token
 var tokens map[int]string
+
+func AddTokens(pid int, token string) {
+	if tokens == nil {
+		tokens = make(map[int]string)
+	}
+	tokens[pid] = token
+}
+
+func RemoveTokens(pid int) {
+	delete(tokens, pid)
+}
+
+func IsEnableToken(pid int, token string) bool {
+	registeredToken, ok := tokens[pid]
+
+	// not exist token or token unmatch
+	return !ok || token != registeredToken
+}
