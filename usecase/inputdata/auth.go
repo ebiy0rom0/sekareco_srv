e5390251be56d761a1f74ec1f98d3f598df2651a
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-07-20 02:14:05 +0900

    [mod]validation update
    delete password policy check

--- usecase/inputdata/auth.go
+++ usecase/inputdata/auth.go
@@ -14,9 +14,6 @@ func (a *PostAuth) Validation() error {
 	if len(a.Password) == 0 {
 		return errors.New("password is required")
 	}
-	if len(a.Password) < 8 {
-		return errors.New("insufficient password security policy")
-	}
 	return nil
 }
 
