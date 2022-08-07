0cad6c7991cf13d217993cdeb6380b30eafe451a
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-08-07 22:09:37 +0900

    [add]data for test

---
+++ test/data/testdata.sql
@@ -0,0 +1,4 @@
+-- password => $2a$12$J1t7JRNU4Dnq2MYLQHIrNOdpRzqD008mwQrnhPdfYdnM9sd3QmPNC
+INSERT INTO person_login(login_id, person_id, password_hash) VALUES ('login_id1', 1, '$2a$12$J1t7JRNU4Dnq2MYLQHIrNOdpRzqD008mwQrnhPdfYdnM9sd3QmPNC');
+INSERT INTO person_login(login_id, person_id, password_hash) VALUES ('login_id2', 2, '$2a$12$J1t7JRNU4Dnq2MYLQHIrNOdpRzqD008mwQrnhPdfYdnM9sd3QmPNC');
+INSERT INTO person_login(login_id, person_id, password_hash) VALUES ('login_id3', 3, '$2a$12$J1t7JRNU4Dnq2MYLQHIrNOdpRzqD008mwQrnhPdfYdnM9sd3QmPNC');
