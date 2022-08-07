e65e30238a080c7cd2365132645c5d706e6d21b2
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-08-07 22:09:11 +0900

    [mod]login unit test

--- interface/database/login_test.go
+++ interface/database/login_test.go
@@ -2,28 +2,64 @@ package database_test
 
 import (
 	"context"
+	"database/sql"
 	"reflect"
 	"sekareco_srv/domain/model"
+	database_ "sekareco_srv/interface/database"
+	"sekareco_srv/test"
 	"sekareco_srv/usecase/database"
 	"testing"
 )
 
+var loginRepo database.LoginRepository
+
+func TestMain(m *testing.M) {
+	test.Setup()
+
+	loginRepo = database_.NewLoginRepository(test.InjectSqlHandler())
+
+	m.Run()
+}
+
+// this database check is unused transaction
 func TestLoginRepository_Store(t *testing.T) {
-	type args struct {
-		ctx context.Context
-		l   model.Login
-	}
+	ctx := context.Background()
 	tests := []struct {
 		name    string
-		r       database.LoginRepository
-		args    args
+		args    model.Login
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "insert successfully",
+			args: model.Login{
+				LoginID:      "login_id4",
+				PersonID:     4,
+				PasswordHash: "$2a$12$J1t7JRNU4Dnq2MYLQHIrNOdpRzqD008mwQrnhPdfYdnM9sd3QmPNC",
+			},
+			wantErr: false,
+		},
+		{
+			name: "duplicate loginID(primary key)",
+			args: model.Login{
+				LoginID:      "login_id1",
+				PersonID:     5,
+				PasswordHash: "$2a$12$J1t7JRNU4Dnq2MYLQHIrNOdpRzqD008mwQrnhPdfYdnM9sd3QmPNC",
+			},
+			wantErr: true,
+		},
+		{
+			name: "duplicate personID(unique key)",
+			args: model.Login{
+				LoginID:      "login_id5",
+				PersonID:     1,
+				PasswordHash: "$2a$12$J1t7JRNU4Dnq2MYLQHIrNOdpRzqD008mwQrnhPdfYdnM9sd3QmPNC",
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.r.Store(tt.args.ctx, tt.args.l); (err != nil) != tt.wantErr {
+			if err := loginRepo.Store(ctx, tt.args); (err != nil) != tt.wantErr {
 				t.Errorf("loginRepository.Store() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -31,28 +67,27 @@ func TestLoginRepository_Store(t *testing.T) {
 }
 
 func TestLoginRepository_GetByID(t *testing.T) {
-	type args struct {
-		ctx     context.Context
-		loginID string
-	}
+	ctx := context.Background()
 	tests := []struct {
-		name      string
-		r         database.LoginRepository
-		args      args
-		wantLogin model.Login
-		wantErr   bool
+		name    string
+		loginID string
+		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{name: "select successfully", loginID: "login_id1", wantErr: false},
+		{name: "not registered loginID", loginID: "login_id5", wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotLogin, err := tt.r.GetByID(tt.args.ctx, tt.args.loginID)
+			gotLogin, err := loginRepo.GetByID(ctx, tt.loginID)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("loginRepository.GetByID() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(gotLogin, tt.wantLogin) {
-				t.Errorf("loginRepository.GetByID() = %v, want %v", gotLogin, tt.wantLogin)
+			if err == sql.ErrNoRows {
+				return
+			}
+			if !reflect.DeepEqual(gotLogin.PersonID, 1) {
+				t.Errorf("loginRepository.GetByID() = %s, want %s", gotLogin.LoginID, tt.loginID)
 			}
 		})
 	}
