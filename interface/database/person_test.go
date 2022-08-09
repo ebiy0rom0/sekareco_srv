9ff9783b8bb9afb23eb6e6a93459041633bf9db1
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-08-09 20:38:19 +0900

    [mod]person repository unit test

--- interface/database/person_test.go
+++ interface/database/person_test.go
@@ -2,6 +2,7 @@ package database_test
 
 import (
 	"context"
+	"database/sql"
 	"reflect"
 	"sekareco_srv/domain/model"
 	"sekareco_srv/usecase/database"
@@ -10,28 +11,42 @@ import (
 
 var personRepo database.PersonRepository
 
+// this database check is unused transaction
 func TestPersonRepository_Store(t *testing.T) {
 	ctx := context.Background()
-	type args struct {
-		p model.Person
-	}
 	tests := []struct {
 		name         string
-		r            database.PersonRepository
-		args         args
+		argPerson    model.Person
 		wantPersonID int
 		wantErr      bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "insert successfully",
+			argPerson: model.Person{
+				PersonName: "p_name04",
+				FriendCode: 1050136379,
+			},
+			wantPersonID: 4,
+			wantErr:      false,
+		},
+		{
+			name: "[another person]insert successfully",
+			argPerson: model.Person{
+				PersonName: "p_name05",
+				FriendCode: 1050136378,
+			},
+			wantPersonID: 5,
+			wantErr:      false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotPersonID, err := tt.r.Store(ctx, tt.args.p)
+			gotPersonID, err := personRepo.Store(ctx, tt.argPerson)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("PersonRepository.Store() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if gotPersonID != tt.wantPersonID {
+			if !tt.wantErr && gotPersonID != tt.wantPersonID {
 				t.Errorf("PersonRepository.Store() = %v, want %v", gotPersonID, tt.wantPersonID)
 			}
 		})
@@ -40,23 +55,46 @@ func TestPersonRepository_Store(t *testing.T) {
 
 func TestPersonRepository_GetByID(t *testing.T) {
 	ctx := context.Background()
-	type args struct {
-		personID int
-	}
 	tests := []struct {
 		name     string
-		r        database.PersonRepository
-		args     args
+		personID int
 		wantUser model.Person
 		wantErr  bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:     "select successfully",
+			personID: 1,
+			wantUser: model.Person{
+				PersonID:   1,
+				PersonName: "name01",
+				FriendCode: 2593519733,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "[another person]select successfully",
+			personID: 4,
+			wantUser: model.Person{
+				PersonID:   4,
+				PersonName: "p_name04",
+				FriendCode: 1050136379,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "un registered person select",
+			personID: 6,
+			wantErr:  true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotUser, err := tt.r.GetByID(ctx, tt.args.personID)
+			gotUser, err := personRepo.GetByID(ctx, tt.personID)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("PersonRepository.GetByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr && err == sql.ErrNoRows {
 				return
 			}
 			if !reflect.DeepEqual(gotUser, tt.wantUser) {
