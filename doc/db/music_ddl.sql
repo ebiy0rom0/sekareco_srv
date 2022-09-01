89f177266b755b4014683d9d89136c27ba0d86a5
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-09-02 01:52:45 +0900

    [add]notes support

--- doc/db/music_ddl.sql
+++ doc/db/music_ddl.sql
@@ -5,10 +5,15 @@ CREATE TABLE IF NOT EXISTS master_music (
     music_name TEXT NOT NULL,
     jacket_url TEXT NOT NULL,
     level_easy INTEGER NOT NULL,
+    notes_easy INTEGER NOT NULL,
     level_normal INTEGER NOT NULL,
+    notes_normal INTEGER NOT NULL,
     level_hard INTEGER NOT NULL,
+    notes_hard INTEGER NOT NULL,
     level_expert INTEGER NOT NULL,
+    notes_expert INTEGER NOT NULL,
     level_master INTEGER NOT NULL,
+    notes_master INTEGER NOT NULL,
     created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
     updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
