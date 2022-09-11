89010a7f3c5445fb3226bd5d765c47635af40d87
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-09-11 23:49:04 +0900

    [mod]comment expansion

--- util/path.go
+++ util/path.go
@@ -5,6 +5,8 @@ import (
 	"runtime"
 )
 
+// RootDir returns the root directory(repository root) path.
+// However, when executing binary returns the 1 level higher path for binary .
 func RootDir() string {
 	_, file, _, _ := runtime.Caller(0)
 	return filepath.Join(filepath.Dir(file), "../")
