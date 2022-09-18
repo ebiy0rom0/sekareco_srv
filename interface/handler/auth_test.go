fadc27120539d6da20521ff6da08b4b2c807cadb
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-09-18 18:10:46 +0900

    [mod]change action trigger

---
+++ interface/handler/auth_test.go
@@ -0,0 +1,66 @@
+package handler
+
+import (
+	"context"
+	"sekareco_srv/interface/infra"
+	"sekareco_srv/usecase/inputport"
+	"testing"
+)
+
+func TestNewAuthHandler(t *testing.T) {
+	type args struct {
+		a inputport.AuthInputport
+	}
+	tests := []struct {
+		name string
+		args args
+		want *authHandler
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// if got := NewAuthHandler(tt.args.a); !reflect.DeepEqual(got, tt.want) {
+			// 	t.Errorf("NewAuthHandler() = %v, want %v", got, tt.want)
+			// }
+		})
+	}
+}
+
+func Test_authHandler_Post(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		hc  infra.HttpContext
+	}
+	tests := []struct {
+		name string
+		h    *authHandler
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.h.Post(tt.args.ctx, tt.args.hc)
+		})
+	}
+}
+
+func Test_authHandler_Delete(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		hc  infra.HttpContext
+	}
+	tests := []struct {
+		name string
+		h    *authHandler
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.h.Delete(tt.args.ctx, tt.args.hc)
+		})
+	}
+}
