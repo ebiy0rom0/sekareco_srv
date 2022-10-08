65dfa1a44cf49b22c4d07e4ba38b72d7795a3b46
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-10-08 16:31:09 +0900

    [mod]tcp health test before connection create

--- infra/sql/mysql.go
+++ infra/sql/mysql.go
@@ -2,17 +2,32 @@ package sql
 
 import (
 	"database/sql"
-	"fmt"
+	"net"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 // openMysql establishes a connection with db for MySQL
 // and returns a pointer to the connection.
 func openMysql(user, pass, host, schema string) (*sql.DB, error) {
 	//user:password@tcp(host:port)/dbname
-	source := fmt.Sprintf(
-		"%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=true",
-		user, pass, host, schema,
-	)
+	// source := fmt.Sprintf(
+	// 	"%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=true",
+	// 	user, pass, host, schema,
+	// )
 
-	return sql.Open("mysql", source)
+	// tcp connection test
+	if _, err := net.DialTimeout("tcp", host, 1 * time.Second); err != nil {
+		return nil, err
+	}
+
+	c := mysql.Config{
+		User:   user,
+		Passwd: pass,
+		Addr:   host,
+		DBName: schema,
+	}
+
+	return sql.Open("mysql", c.FormatDSN())
 }
