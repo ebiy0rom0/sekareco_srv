802c2af78cb5167ae7b4dda8224abb2e0e7ceac2
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2023-03-02 02:25:42 +0900

    [mod]fix handler exec test
    proper body inspection

--- infra/web/http_handler_test.go
+++ infra/web/http_handler_test.go
@@ -2,6 +2,8 @@ package web
 
 import (
 	"context"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -10,30 +12,68 @@ import (
 )
 
 func TestHttpHandler_Exec(t *testing.T) {
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/handler_test", nil)
-	t.Run("Exec do execute function", func(t *testing.T) {
-		HttpHandler(func(ctx context.Context, httpCtx infra.HttpContext) *infra.HttpError {
-			httpCtx.Response(http.StatusNotFound, "")
-			return nil
-		}).Exec(w, r)
-	})
+	{
+		type message struct {
+			Msg string `json:"message"`
+		}
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("GET", "/handler_test", nil)
+		wantMsg := "ok"
+		t.Run("Exec do execute function", func(t *testing.T) {
+			HttpHandler(func(ctx context.Context, httpCtx infra.HttpContext) *infra.HttpError {
+				body := message{Msg: wantMsg}
+				httpCtx.Response(http.StatusNotFound, body)
+				return nil
+			}).Exec(w, r)
+		})
 
-	if w.Result().StatusCode != http.StatusNotFound {
-		t.Errorf("HttpHandler.Exec() not executed: status=%d", w.Result().StatusCode)
+		if w.Result().StatusCode != http.StatusNotFound {
+			t.Errorf("HttpHandler.Exec() not executed: status=%d", w.Result().StatusCode)
+		}
+		defer w.Result().Body.Close()
+		b, err := io.ReadAll(w.Result().Body)
+		if err != nil {
+			t.Errorf("failed to read request body. %s", err)
+			return
+		}
+		var body message
+		if err := json.Unmarshal(b, &body); err != nil {
+			t.Errorf("failed to marshal request body. %s", err)
+			return
+		}
+		if !reflect.DeepEqual(body, message{Msg: wantMsg}) {
+			t.Errorf("Different body formats. body=%+v", body)
+		}
 	}
+	{
+		type errMessage struct {
+			Err string `json:"error"`
+		}
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("GET", "/handler_test", nil)
+		wantErr := "page not found"
+		t.Run("Exec returns HttpError", func(t *testing.T) {
+			HttpHandler(func(ctx context.Context, httpCtx infra.HttpContext) *infra.HttpError {
+				return &infra.HttpError{Msg: wantErr, Code: http.StatusNotFound}
+			}).Exec(w, r)
+		})
+		defer w.Result().Body.Close()
 
-	wantErr := "page not found"
-	t.Run("Exec returns HttpError", func(t *testing.T) {
-		HttpHandler(func(ctx context.Context, httpCtx infra.HttpContext) *infra.HttpError {
-			return &infra.HttpError{Msg: wantErr, Code: http.StatusNotFound}
-		}).Exec(w, r)
-	})
-
-	if w.Result().StatusCode != http.StatusNotFound {
-		t.Errorf("Unmatch returns http status: status=%d", w.Result().StatusCode)
-	}
-	if !reflect.DeepEqual(w.Result().Body, struct{ Error string }{Error: wantErr}) {
-		t.Errorf("Different body formats. body=%v", w.Result().Body)
+		if w.Result().StatusCode != http.StatusNotFound {
+			t.Errorf("Unmatch returns http status: status=%d", w.Result().StatusCode)
+		}
+		b, err := io.ReadAll(w.Result().Body)
+		if err != nil {
+			t.Errorf("failed to read request body. %s", err)
+			return
+		}
+		var body errMessage
+		if err := json.Unmarshal(b, &body); err != nil {
+			t.Errorf("failed to marshal request body. %s", err)
+			return
+		}
+		if !reflect.DeepEqual(body, errMessage{Err: wantErr}) {
+			t.Errorf("Different body formats. body=%+v", body)
+		}
 	}
 }
